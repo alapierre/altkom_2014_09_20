5c64b07be1ce5436485ea09fe81ec27e2c270a02
student <student@Max19>
2014-09-20 13:36:41 +0200

    Apteka + Recepta wersja 1

---
+++ src/hospital/MedicamentStory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hospital;
+
+/**
+ *
+ * @author student
+ */
+public class MedicamentStory {
+    //aptekarz
+    Recipe recipe = new Recipe(); //recepta
+    //lekarstwo
+    
+    public MedicamentStory() {
+    }
+
+    @Override
+    public String toString() {
+        return "MedicamentStory{" + '}';
+    }
+    
+    
+}
---
+++ src/hospital/Recipe.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hospital;
+
+import java.util.Date;
+
+/**
+ *
+ * @author student
+ */
+public class Recipe {
+    //lekarstwo
+    String dosage; //dawkowanie
+    //pacjent
+    //Doctor doctor = new Doctor();
+    Date date = new Date();//data wypisania recepty
+
+    public Recipe() {
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" + "dosage=" + dosage + ", date=" + date + '}';
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+}
