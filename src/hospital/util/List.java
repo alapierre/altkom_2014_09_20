33b9e2f5cddeff8865d4c100fdc0a972f44bf321
Adrian Lapierre <adrian@soft-project.pl>
2014-09-21 11:18:01 +0200

    kolekcja

--- src/hospital/util/List.java
+++ src/hospital/util/List.java
@@ -8,6 +8,29 @@ package hospital.util;
  *
  * @author student
  */
-public class List {
+public class List implements Storage {
+
+    private Object[] tab = new Object[10];
+    private int current;
     
+    @Override
+    public void add(Object item) {
+        if(current >= tab.length ) {
+            Object[] tmp = new Object[tab.length + 10];
+            System.arraycopy(tab, 0, tmp, 0, tab.length);
+            tab = tmp;
+        } 
+        
+        tab[current++] = item;
+        
+    }
+
+    @Override
+    public Object get(int idx) {
+        return tab[idx];
+    }
+    
+    public int size() {
+        return current + 1;
+    }
 }
