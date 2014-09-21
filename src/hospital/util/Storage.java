14d3d53bc96fbc89db22962e71cbd274c0c347f0
Adrian Lapierre <adrian@soft-project.pl>
2014-09-21 10:35:42 +0200

    interfejs kolekcji

---
+++ src/hospital/util/Storage.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital.util;
+
+/**
+ *
+ * @author student
+ */
+public interface Storage {
+    
+    public void add(Object item);
+    
+    public Object get(int idx);
+    
+}
