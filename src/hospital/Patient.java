66b5de31269e44a186307e058db1b64d19c0f834
student <student@Max4>
2014-09-21 09:33:42 +0200

    additional fields in class Patient

--- src/hospital/Patient.java
+++ src/hospital/Patient.java
@@ -16,19 +16,24 @@ public class Patient extends Person {
     ArrayList<Symptom> symptoms = new ArrayList<Symptom>();    
     ArrayList<Object> history = new ArrayList<Object>();    
     ArrayList<Object> alergies = new ArrayList<Object>();
-    
+       
     Object insurance;
     Doctor attendingDoctor;
     Object ward;
     
-    public void addSymptom()
+    Date dateOfAdmission;
+    Date dateOfDischarge;
+    
+    public void addSymptom(Symptom symptom)
     {
-        
+        symptoms.add(symptom);
     }
     
-    
-    
-    
+    public ArrayList<Symptom> getSymptoms()
+    {
+        return this.symptoms;
+    }
+        
     public void payBill() {
              
     }
@@ -37,12 +42,81 @@ public class Patient extends Person {
              
     }
     
-    public void dischargeFromHospital(Date date)
-    {
-        
-    }
     public void admitToHospital(Date date)
     {
-        
+        setDateOfAdmission(date);
+    }
+    
+    public void dischargeFromHospital(Date date)
+    {
+        setDateOfDischarge(date);
+    }
+    
+    
+    /*********** Getters ***********/    
+    
+    
+    public ArrayList<Object> getHistory() {
+        return history;
+    }
+
+    public ArrayList<Object> getAlergies() {
+        return alergies;
+    }
+
+    public Object getInsurance() {
+        return insurance;
+    }
+
+    public Doctor getAttendingDoctor() {
+        return attendingDoctor;
+    }
+
+    public Object getWard() {
+        return ward;
+    }
+
+    public Date getDateOfAdmission() {
+        return dateOfAdmission;
+    }
+
+    public Date getDateOfDischarge() {
+        return dateOfDischarge;
+    }
+
+    
+    /*********** Setters ***********/ 
+    
+    
+    public void setSymptoms(ArrayList<Symptom> symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public void setHistory(ArrayList<Object> history) {
+        this.history = history;
+    }
+
+    public void setAlergies(ArrayList<Object> alergies) {
+        this.alergies = alergies;
+    }
+
+    public void setInsurance(Object insurance) {
+        this.insurance = insurance;
+    }
+
+    public void setAttendingDoctor(Doctor attendingDoctor) {
+        this.attendingDoctor = attendingDoctor;
+    }
+
+    public void setWard(Object ward) {
+        this.ward = ward;
+    }
+
+    private void setDateOfAdmission(Date dateOfAdmission) {
+        this.dateOfAdmission = dateOfAdmission;
+    }
+    
+     private void setDateOfDischarge(Date dateOfDischarge) {
+        this.dateOfDischarge = dateOfDischarge;
     }
 }
